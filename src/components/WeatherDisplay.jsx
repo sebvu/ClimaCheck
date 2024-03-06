b815f999878cf383cacc3ff92a10537a6639e500
Jester Santos <santosjester738@gmail.com>
2024-03-06 13:56:09 -0600

    Implement basic weather display
    Created weather display that shows current temperature, weather summary,
    date and current city being displayed.

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { PiDogDuotone } from "react-icons/pi";
 
-const WeatherDisplay = ({weather}) => {
+const formatDate = (date) => {
+  const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+  }
+  return new Date(date).toLocaleString('en-US', options);
+}
+
+const WeatherDisplay = ({city, state, weather}) => {
   return (
-    <div>
-      <h2>hello</h2>
-    </div>
+    <>
+      <div className='bg-transparent h-3/6 w-full flex justify-center items-center'>
+        <PiDogDuotone size = '250'/> {/* placeholder */}
+      </div>
+      <div className='bg-transparent h-1/6 w-full flex justify-center items-center flex-col'>
+        <div className='m-2 text-3xl font-semibold'>
+          <h1>{weather.current.temp + "°F"}</h1>
+        </div>
+        <div className='m-2 text-xl'>
+          <p>{weather.daily[0].summary}</p>
+        </div>
+      </div>
+      <div className='bg-transparent h-2/6 w-full flex justify-center items-center flex-col'>
+        <div>
+
+        </div>
+        <div className='m-3 text-xl'>
+          <p>{formatDate((weather.current.dt)*1000)}</p>
+        </div>
+        <div className='m-2 font-thin text-3xl'>
+          <h2>{city}, {state}</h2>
+        </div>
+      </div>
+    </>
+
   )
 }
 
